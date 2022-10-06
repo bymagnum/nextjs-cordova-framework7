417f1fc19f3d385e00b9c695df0bd09f75a76422
Pavel Krestyannikov <pavlok2008@yandex.ru>
2022-10-06 05:33:22 +0300

    update

--- dist/bin/create-app.js
+++ dist/bin/create-app.js
@@ -4,27 +4,27 @@ var path = require('path');
 
 async function CreateApp () {
 
-    if (fs.existsSync('dist/template/components')) {
+    if (fse.existsSync('dist/template/components')) {
         fse.copySync('dist/template/components', 'components');
     }
 
-    if (fs.existsSync('dist/template/pages')) {
+    if (fse.existsSync('dist/template/pages')) {
         fse.copySync('dist/template/pages', 'pages');
     }
 
-    if (fs.existsSync('dist/template/config.xml')) {
+    if (fse.existsSync('dist/template/config.xml')) {
         fse.copySync('dist/template/config.xml', 'config.xml');
     }
 
-    if (fs.existsSync('dist/template/next.config.js')) {
+    if (fse.existsSync('dist/template/next.config.js')) {
         fse.copySync('dist/template/next.config.js', 'next.config.js');
     }
  
-    if (fs.existsSync('dist/template/.env.development')) {
+    if (fse.existsSync('dist/template/.env.development')) {
         fse.copySync('dist/template/.env.development', '.env.development');
     }
 
-    if (fs.existsSync('dist/template/.env.production')) {
+    if (fse.existsSync('dist/template/.env.production')) {
         fse.copySync('dist/template/.env.production', '.env.production');
     }
 
